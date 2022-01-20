8c8b2171e3488c375a345044099f5a76c6fa6984
HYEBIN,PARK <hyeqls3742@gmail.com>
2022-01-20 20:52:33 +0900

    spring security config

---
+++ src/main/java/com/example/smallshop/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.example.smallshop.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(); //해시함수=> 비밀번호를 암호화 하여 저장
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+
+    }
+}
