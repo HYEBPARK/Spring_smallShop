1333550b5cfe508f14a195b7c3f4dd8332aa0a72
HYEBIN,PARK <hyeqls3742@gmail.com>
2022-01-20 20:53:51 +0900

    회원 가입 기능

--- src/main/java/com/example/smallshop/controller/MemberController.java
+++ src/main/java/com/example/smallshop/controller/MemberController.java
@@ -7,11 +7,14 @@ import com.example.smallshop.domain.MemberForm;
 import com.example.smallshop.service.ItemService;
 import com.example.smallshop.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
 import java.util.List;
 
 
@@ -20,6 +23,7 @@ import java.util.List;
 public class MemberController {
 
     private final MemberService memberService;
+    private final PasswordEncoder passwordEncoder;
 
     @GetMapping("/member")
     public String createMember(Model model){
@@ -27,10 +31,24 @@ public class MemberController {
         return "member/createMemberForm";
     }
 
-    @PostMapping("/member")
-    public String create(MemberForm memberForm){
-        Member member = Member.createMember(memberForm);
-        memberService.saveMember(member);
+//    @PostMapping("/member")
+//    public String create(MemberForm memberForm){
+//       Member member = Member.createMember(memberForm, passwordEncoder);
+//       memberService.saveMember(member);
+//       return "redirect:/";
+//    }
+
+    @PostMapping(value = "/member")
+    public String reMember(@Valid MemberForm memberForm, BindingResult bindingResult, Model model){
+        if(bindingResult.hasErrors()) return "member/createMemberForm";
+        try {
+            Member member = Member.createMember(memberForm,passwordEncoder);
+            memberService.saveMember(member);
+        } catch (IllegalStateException e){
+            model.addAttribute("errorMessage", e.getMessage());
+            return "member/createMemberForm";
+        }
+
         return "redirect:/";
     }
 
