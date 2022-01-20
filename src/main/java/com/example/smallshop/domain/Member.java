e997c6ca40f92bd133a1bcf7c4ed525b24772a15
HYEBIN,PARK <hyeqls3742@gmail.com>
2022-01-20 20:53:26 +0900

    password Encode

--- src/main/java/com/example/smallshop/domain/Member.java
+++ src/main/java/com/example/smallshop/domain/Member.java
@@ -4,6 +4,7 @@ package com.example.smallshop.domain;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.persistence.*;
 
@@ -26,11 +27,12 @@ public class Member {
     @Enumerated(EnumType.STRING)
     private Role role;
 
-    public static Member createMember(MemberForm memberForm){
+    public static Member createMember(MemberForm memberForm, PasswordEncoder passwordEncoder){
         Member member = new Member();
         member.setName(memberForm.getName());
         member.setEmail(memberForm.getEmail());
-        member.setPassword(memberForm.getPassword());
+        String password = passwordEncoder.encode(memberForm.getPassword());
+        member.setPassword(password);
         member.setRole(Role.USER);
         return member;
     }
