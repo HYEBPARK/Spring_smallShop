2a2ab318928cb6941c802936d3b3c19d23703284
HYEBIN,PARK <hyeqls3742@gmail.com>
2022-01-20 20:52:57 +0900

    validation, 값검증

--- src/main/java/com/example/smallshop/domain/MemberForm.java
+++ src/main/java/com/example/smallshop/domain/MemberForm.java
@@ -4,13 +4,25 @@ package com.example.smallshop.domain;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 
 
 @Getter
 @Setter
 @ToString
 public class MemberForm {
+
+    @NotBlank(message = "이름을 입력해주세요")
     private String name;
+
+    @NotBlank(message = "이메일을 입력해주세요")
+    @Email(message = "이메일 형식이 유효하지 않습니다.")
     private String email;
+
+    @NotBlank(message = "비밀번호를 입력해주세요")
+    @Length(min = 8, max =12, message = "비밀번호는 8자 이상, 16자 이하로 입력해주세요")
     private String password;
 }
